426bccbc44ceb509872c4583e791db94a845c12d
Zachary Resnick <zacharyresnick@Zacharys-MBP.fios-router.home>
2023-04-09 12:46:49 -0400

    Updates Paths

--- run-app.sh
+++ run-app.sh
@@ -5,13 +5,13 @@ if [[ -n flask_app_pid ]]; then
 	kill -9 $flask_app_pid;
 fi;
 
-cd /photo-downloader-master/
+cd ~/Desktop/SmugMugScraper/photo-downloader-master/
 python3 -m venv venv
 . venv/bin/activate
 source venv/bin/activate
 pip3 install -r requirements.txt
 flask run --debug -p5111 &
-cd ../photo-downloader-frontend-master/
+cd ~/Desktop/SmugMugScraper/photo-downloader-frontend-master/
 nvm install 14.15.5
 nvm use 14.15.5 
 npm install
